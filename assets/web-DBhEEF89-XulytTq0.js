69b003dcdfa8a6152cfbd64e1f7870112ad2f770
aleblll <aleblll@users.noreply.github.com>
2026-09-08 18:51:34 +0400

    Deploy refined 3-ФАИД-110 schedule updates to GitHub Pages

---
+++ assets/web-DBhEEF89-XulytTq0.js
@@ -0,0 +1,1 @@
+import{u as t}from"./index-5ARH2bvT.js";class i extends t{async canShare(){return typeof navigator>"u"||!navigator.share?{value:!1}:{value:!0}}async share(a){if(typeof navigator>"u"||!navigator.share)throw this.unavailable("Share API not available in this browser");return await navigator.share({title:a.title,text:a.text,url:a.url}),{}}}export{i as ShareWeb};
